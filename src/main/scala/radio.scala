df7d8d4ac9dee0f124ecdff21318bccdfec93cce
Kyo91 <sam.sam.42@gmail.com>
2013-07-05 08:22:49 -0400

    Added license information

--- src/main/scala/radio.scala
+++ src/main/scala/radio.scala
@@ -1,3 +1,19 @@
+/*  R-a-dioBot - A simple Twitter bot for R/a/dio <http://r-a-d.io/>
+   Copyright (C) 2013 Kyo91
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 import twitter4j._
 import collection.JavaConversions._
 import java.net.URL
